9770797f41265cd9e96cb7536e95ff6669ee43b7
UnGast <zadrian@web.de>
2020-07-29 15:20:30 +0200

    add Center Widget

--- Sources/WidgetGUI/Widgets/Standard/Center.swift
+++ Sources/WidgetGUI/Widgets/Standard/Center.swift
@@ -1,3 +1,21 @@
-/*public class Center: SingleChildWidget {
+import CustomGraphicsMath
 
-}*/
+public class Center: SingleChildWidget {
+    private var inputChild: Widget
+    
+    public init(@WidgetBuilder child: () -> Widget) {
+        self.inputChild = child()
+        super.init()
+    }
+
+    override open func buildChild() -> Widget {
+        inputChild
+    }
+
+    override open func layout() {
+        child.constraints = BoxConstraints(minSize: DSize2.zero, maxSize: constraints!.maxSize)
+        child.layout()
+        bounds.size = constraints!.constrain(child.bounds.size)
+        child.bounds.topLeft = DVec2(bounds.size - child.bounds.size) / 2
+    }
+}
