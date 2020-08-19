06cd6c0baeeb52ae9ca3a02ff55c5de1742f7ae0
UnGast <zadrian@web.de>
2020-08-19 10:31:22 +0200

    add new initializer to ComputedSize

--- Sources/WidgetGUI/Widgets/Standard/ComputedSize.swift
+++ Sources/WidgetGUI/Widgets/Standard/ComputedSize.swift
@@ -2,11 +2,16 @@ import VisualAppBase
 import CustomGraphicsMath
 
 public class ComputedSize: SingleChildWidget {
+    public enum DimensionUnitValue {
+        case Pixels(_ value: Double)
+        case Percent(_ value: Double)
+    }
+
     public typealias CalculationFunction = (_ constraints: BoxConstraints) -> BoxConstraints
 
     public var calculate: CalculationFunction
 
-    private var inputChild: Widget
+    private var childBuilder: () -> Widget
 
     /*public init(calculate: @escaping CalculationFunction, child inputChild: Widget) {
         self.calculate = calculate
@@ -14,15 +19,43 @@ public class ComputedSize: SingleChildWidget {
         super.init()
     }*/
 
-    public init(@WidgetBuilder child inputChild: () -> Widget, calculate: @escaping CalculationFunction) {
+    public init(@WidgetBuilder child childBuilder: @escaping () -> Widget, calculate: @escaping CalculationFunction) {
         //self.init(calculate: calculate, child: inputChild())
         self.calculate = calculate
-        self.inputChild = inputChild()
+        self.childBuilder = childBuilder
         super.init()
     }
 
+    public convenience init(width: DimensionUnitValue? = nil, height: DimensionUnitValue? = nil, @WidgetBuilder child childBuilder: @escaping () -> Widget) {
+        self.init(child: childBuilder, calculate: {
+            let resultWidth: Double?
+            switch width {
+            case let .Pixels(value):
+                resultWidth = value
+            case let .Percent(value):
+                resultWidth = $0.maxSize.width * (value / 100)
+            default:
+                resultWidth = nil
+            }
+
+            let resultHeight: Double?
+            switch height {
+            case let .Pixels(value):
+                resultHeight = value
+            case let .Percent(value):
+                resultHeight = $0.maxSize.height * (value / 100)
+            default:
+                resultHeight = nil
+            }
+
+            return BoxConstraints(
+                minSize: DSize2(resultWidth ?? $0.minSize.width, resultHeight ?? $0.minSize.height),
+                maxSize: DSize2(resultWidth ?? $0.maxSize.width, resultHeight ?? $0.maxSize.height))
+        })
+    }
+
     override open func buildChild() -> Widget {
-        inputChild
+        childBuilder()
     }
 
     override open func performLayout() {
