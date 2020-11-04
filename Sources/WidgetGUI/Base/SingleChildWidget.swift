db907edf460ff1deea6304c030ac1135bdcc9a8c
Adrian Zimmermann <zadrian@web.de>
2020-11-04 11:15:13 +0100

    improve performance by rebuilding SingleChildWidget only in onTick

--- Sources/WidgetGUI/Base/SingleChildWidget.swift
+++ Sources/WidgetGUI/Base/SingleChildWidget.swift
@@ -5,6 +5,8 @@ import VisualAppBase
 open class SingleChildWidget: Widget {
   open lazy var child: Widget = buildChild()
 
+  private var nextTickRebuiltScheduled: Bool = false
+
   override open func build() {
     children = [child]
   }
@@ -18,16 +20,22 @@ open class SingleChildWidget: Widget {
       return
     }
 
-    child = buildChild()
-    replaceChildren(with: [child])
+    if !nextTickRebuiltScheduled {
+      nextTickRebuiltScheduled = true
+      nextTick { [weak self] _ in
+        if let self = self {
+          self.child = self.buildChild()
+          self.replaceChildren(with: [self.child])
+          self.nextTickRebuiltScheduled = false
+        }
+      }
+    }
+    // TODO: should there be an invalidateChildren() / invalidateBuild on Widget in general
+    // and the widgets be rebuilt later, and in a way, that only the topmost ones are rebuilt? 
+
+    // TODO: should the rest of flag functions also be fired in on click?
+
     invalidateBoxConfig()
-
-    // TODO: make sure layout is not performed twice shortly after another
-    // it is probably already called by invalidateBoxConfig
-    // have a flag to show that layout is up to date already
-    //layoutInvalid = true
-
-    //layout(constraints: previousConstraints!)
     invalidateLayout()
     invalidateRenderState()
   }
