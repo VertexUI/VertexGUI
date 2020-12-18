013200ee111aa724bb4e180ef3629c5114fc9105
Adrian Zimmermann <zadrian@web.de>
2020-12-18 21:01:29 +0100

    add test for styles on dynamically inserted Widgets

--- Sources/VisualAppBase/rendering/primitives/Tick.swift
+++ Sources/VisualAppBase/rendering/primitives/Tick.swift
@@ -1,13 +1,9 @@
 public struct Tick: Equatable {
-
     public let deltaTime: Double
-
     public let totalTime: Double
 
     public init(deltaTime: Double, totalTime: Double) {
-        
         self.deltaTime = deltaTime
-        
         self.totalTime = totalTime
     }
 }
--- Tests/WidgetGUITests/StyleTests.swift
+++ Tests/WidgetGUITests/StyleTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import VisualAppBase
 @testable import WidgetGUI
 
 final class StyleTests: XCTestCase {
@@ -56,11 +57,12 @@ final class StyleTests: XCTestCase {
     XCTAssertFalse(style1 == style9)
   }
 
-  func testSimpleStyles() {
+  func testStyleSelectorAndOrderRespected() {
     let widget1 = ExperimentalText("Test1").with(classes: ["class-1"])
     let widget2 = ExperimentalText("Test2").with(classes: ["class-2"])
     let widget3 = Text("Test3").with(classes: ["class-2"])
     let widget4 = Text("Test4").with(classes: ["class-3"])
+    let widget5 = ExperimentalText("Test5").with(classes: ["class-1", "class-2", "class-3"])
     let class2Style1 = ExperimentalText.Style(WidgetSelector(classes: ["class-2"])) {
       $0.foreground = .grey
     }
@@ -72,6 +74,7 @@ final class StyleTests: XCTestCase {
       widget2
       widget3
       widget4
+      widget5
     }.provideStyles {
       ExperimentalText.Style(WidgetSelector(classes: ["class-1"])) {
         $0.fontSize = 50
@@ -87,14 +90,50 @@ final class StyleTests: XCTestCase {
     XCTAssertEqual(widget2.appliedStyles.count, 2)
     XCTAssertEqual(widget3.appliedStyles.count, 2)
     XCTAssertEqual(widget4.appliedStyles.count, 0)
+    XCTAssertEqual(widget5.appliedStyles.count, 3)
 
     // check order of styles
     XCTAssert(widget2.appliedStyles[0] == class2Style1)
     XCTAssert(widget2.appliedStyles[1] == class2Style2)
   }
 
+  func testStyleOnDynamicallyInsertedWidget() {
+    let widget1 = ExperimentalText("Text1").with(classes: ["class-1"])
+    let widget2 = ExperimentalText("Text2").with(classes: ["class-2"])
+
+    var showWidget2 = false
+
+    let rootWidget = Column {
+      widget1
+      if showWidget2 {
+        widget2
+      }
+    }.provideStyles {
+      ExperimentalText.Style(WidgetSelector(classes: ["class-1"])) {
+        $0.fontSize = 30
+      }
+      ExperimentalText.Style(WidgetSelector(classes: ["class-2"])) {
+        $0.fontWeight = .bold
+      }
+    }
+
+    let mockRoot = MockRoot(rootWidget: rootWidget)
+
+    XCTAssertEqual(widget1.appliedStyles.count, 1)
+    XCTAssertEqual(widget2.appliedStyles.count, 0)
+
+    showWidget2 = true
+
+    rootWidget.invalidateBuild()
+    mockRoot.tick(Tick(deltaTime: 0, totalTime: 0))
+
+    XCTAssertEqual(widget1.appliedStyles.count, 1)
+    XCTAssertEqual(widget2.appliedStyles.count, 1)
+  }
+
   static var allTests = [
     ("testStyleComparison", testStyleComparison),
-    ("testSimpleStyles", testSimpleStyles)
+    ("testStyleOnDynamicallyInsertedWidget", testStyleOnDynamicallyInsertedWidget),
+    ("testStyleSelectorAndOrderRespected", testStyleSelectorAndOrderRespected)
   ]
 }
