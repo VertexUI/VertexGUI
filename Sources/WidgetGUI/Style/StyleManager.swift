ad541e211fb509daf383f81dc18f74469dce306b
Adrian Zimmermann <zadrian@web.de>
2020-12-18 04:46:05 +0100

    implement basic style managing

--- Sources/WidgetGUI/Style/StyleManager.swift
+++ Sources/WidgetGUI/Style/StyleManager.swift
@@ -1,47 +1,56 @@
 public class StyleManager {
   private let rootWidget: Widget 
-  
-  private var styles: [StyleWrapper] = []
 
   public init(rootWidget: Widget) {
     self.rootWidget = rootWidget 
   }
 
   public func setup() {
-    retrieveStyles(rootWidget)
+    processTree(rootWidget)
   }
 
   public func refresh(_ widgets: [Widget]) {
     for widget in widgets {
-      retrieveStyles(widget)
+      processTree(widget)
     }
   }
 
-  public func retrieveStyles(_ initialWidget: Widget) {
-    var iterators = [initialWidget.visitChildren()]
+  public func processTree(_ initialWidget: Widget) {
+    var parentStyles = [StyleWrapper]()
 
-    while var iterator = iterators.last {
-      if let widget = iterator.next() {
-        styles.append(contentsOf: widget.providedStyles.map { StyleWrapper(style: $0, source: widget) })
-        iterators[iterators.count - 1] = iterator // to store the advancement by next()
-        iterators.append(widget.visitChildren())
-        continue
-      } else {
-        iterators.removeLast()
+    var nextParent = Optional(initialWidget)
+    while let currentParent = nextParent {
+      parentStyles.append(contentsOf: currentParent.providedStyles.map { StyleWrapper(style: $0, source: currentParent) })
+      nextParent = currentParent.parent as? Widget 
+    }
+
+    applyStyles(parentStyles, to: initialWidget)
+
+    var iterators: [(iterator: Widget.ChildIterator, branchStyles: [StyleWrapper])] = [(iterator: initialWidget.visitChildren(), branchStyles: parentStyles)]
+
+    while var (iterator, branchStyles) = iterators.first {
+      while let widget = iterator.next() {
+        let widgetProvidedStyles = widget.providedStyles.map { StyleWrapper(style: $0, source: widget) }
+        iterators.append((iterator: widget.visitChildren(), branchStyles: branchStyles + widgetProvidedStyles))
+        applyStyles(branchStyles + widgetProvidedStyles, to: widget) 
+      }
+      iterators.removeFirst()
+    }
+  }
+
+  public func applyStyles(_ styleWrappers: [StyleWrapper], to widget: Widget) {
+    widget.appliedStyles = []
+    for styleWrapper in styleWrappers {
+      if styleWrapper.style.selector == nil || styleWrapper.style.selector!.selects(widget) {
+        widget.appliedStyles.append(styleWrapper.style)
       }
     }
-
-    print("RETRIEVED STYLES", styles)
-  }
-
-  public func applyStyles() {
-
   }
 }
 
 public struct StyleWrapper {
-  private let style: AnyStyle
-  private let source: Widget
+  public let style: AnyStyle
+  public let source: Widget
 
   public init(style: AnyStyle, source: Widget) {
     self.style = style
