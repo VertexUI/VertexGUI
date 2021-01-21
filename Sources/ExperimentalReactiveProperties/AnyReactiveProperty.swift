4bfd9dd5496628b2f2ee813c1ad7318e6568f25e
UnGast <zadrian@web.de>
2021-01-21 01:21:30 +0100

    add debug id to all ReactiveProperties

--- Sources/ExperimentalReactiveProperties/AnyReactiveProperty.swift
+++ Sources/ExperimentalReactiveProperties/AnyReactiveProperty.swift
@@ -1,6 +1,12 @@
 import Events
 
+private var reactivePropertyIds = [ObjectIdentifier: UInt]()
+private var nextReactivePropertyId: UInt = 0
+
 public protocol AnyReactiveProperty: class {
+  /** used for debugging */
+  var id: UInt { get }
+
   var onAnyChanged: EventHandlerManager<(old: Any, new: Any)> { get }
 
   /** Flag to indicate whether the value of the property has been initialized and can be read. */
@@ -16,6 +22,14 @@ public protocol AnyReactiveProperty: class {
 }
 
 extension AnyReactiveProperty {
+  public var id: UInt {
+    if reactivePropertyIds[ObjectIdentifier(self)] == nil {
+      reactivePropertyIds[ObjectIdentifier(self)] = nextReactivePropertyId
+      nextReactivePropertyId += 1
+    } 
+    return reactivePropertyIds[ObjectIdentifier(self)]!
+  }
+
   public func registerBinding(_ binding: PropertyBindingProtocol) -> () -> () {
     registeredBindings.append(binding)
     return { [weak self, weak binding] in
