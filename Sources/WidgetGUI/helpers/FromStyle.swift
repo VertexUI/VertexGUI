f70b3e86684b73a2b404fff48cdf130e971a266e
UnGast <zadrian@web.de>
2021-02-08 19:12:06 +0100

    add ReactiveProperty projection to FromStyle

--- Sources/WidgetGUI/helpers/FromStyle.swift
+++ Sources/WidgetGUI/helpers/FromStyle.swift
@@ -19,9 +19,19 @@ public class FromStyle<Value: StyleValue>: FromStyleProtocol {
     }
   }
 
+  private var _projectedValue: ExperimentalReactiveProperties.ComputedProperty<Value>
+  public var projectedValue: ExperimentalReactiveProperties.ComputedProperty<Value> {
+    _projectedValue
+  }
+
   public init(wrappedValue defaultValue: Value, key: StyleKey) {
     self.key = key
     self.defaultValue = defaultValue
+
+    self._projectedValue = ExperimentalReactiveProperties.ComputedProperty()
+    self._projectedValue.reinit(compute: { [unowned self] in
+      wrappedValue
+    }, dependencies: [$value])
   }
 
   internal func registerWidget(_ widget: Widget) {
