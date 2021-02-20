6a13dfcddece25ea52ed4e3ca761f4f0215ab63b
UnGast <zadrian@web.de>
2021-02-20 08:49:40 +0100

    give fontSize default value inherit

--- Sources/WidgetGUI/Style/DefaultStylePropertySupportDefinitions.swift
+++ Sources/WidgetGUI/Style/DefaultStylePropertySupportDefinitions.swift
@@ -4,4 +4,5 @@ import VisualAppBase
 let defaultStylePropertySupportDefinitions = StylePropertySupportDefinitions {
   (AnyDefaultStyleKeys.foreground, type: .specific(Color.self), default: SpecialStyleValue.inherit)
   (AnyDefaultStyleKeys.fontFamily, type: .specific(FontFamily.self), default: SpecialStyleValue.inherit)
+  (AnyDefaultStyleKeys.fontSize, type: .specific(Double.self), default: SpecialStyleValue.inherit)
 }
