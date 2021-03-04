c49aa503fe3dab74060ef2e1b3a695db4f5d6f6c
UnGast <zadrian@web.de>
2021-03-04 22:18:14 +0100

    make Dynamic accept a Publisher as trigger

--- Sources/WidgetGUI/Composition/Dynamic.swift
+++ Sources/WidgetGUI/Composition/Dynamic.swift
@@ -1,5 +1,6 @@
 import ReactiveProperties
 import Events
+import CXShim
 
 public class Dynamic<C: ExpContentProtocol> {
   var associatedStyleScope: UInt
@@ -8,6 +9,8 @@ public class Dynamic<C: ExpContentProtocol> {
 
   var dependencyProxies: [AnyReactiveProperty]
   let onDependenciesChanged = EventHandlerManager<Void>()
+
+  var triggerSubscription: AnyCancellable?
 
   // have dependency: as part of function signature
   // because the specific initializers in the extensions
@@ -37,8 +40,28 @@ public class Dynamic<C: ExpContentProtocol> {
     })
   }
 
-  deinit {
-    print("DEINIT DYNAMIC")
+  private init<P: Publisher>(trigger: P, build: @escaping () -> [C.Partial]) where P.Failure == Never {
+    self.dependencyProxies = []
+
+    self.associatedStyleScope = Widget.activeStyleScope
+
+    let partials = build()
+    self.content = C(partials: partials)
+    
+    triggerSubscription = trigger.sink { [unowned self] _ in
+      onDependenciesChanged.invokeHandlers()
+
+      Widget.inStyleScope(self.associatedStyleScope) {
+        self.content.partials = build()
+      }
+    }
+
+    // don't need to update content anymore after it is destroyed
+    // it's necessary to manual destroy because this Dynamic object
+    // is captured within the handler, to avoid early deallocation
+    _ = content.onDestroy({ [self] in
+      self.triggerSubscription?.cancel()
+    })
   }
 }
 
@@ -46,10 +69,18 @@ extension Dynamic where C == ExpDirectContent {
   public convenience init<P: ReactiveProperty>(_ dependency: P, @ExpDirectContentBuilder build: @escaping () -> [C.Partial]) {
     self.init(dependency: dependency, build: build)
   }
+
+  public convenience init<P: Publisher>(_ trigger: P, @ExpDirectContentBuilder build: @escaping () -> [C.Partial]) where P.Failure == Never {
+    self.init(trigger: trigger, build: build)
+  }
 }
 
 extension Dynamic where C == ExpSlottingContent {
   public convenience init<P: ReactiveProperty>(_ dependency: P, @ExpSlottingContentBuilder build: @escaping () -> [C.Partial]) {
     self.init(dependency: dependency, build: build)
+  }
+
+  public convenience init<P: Publisher>(_ trigger: P, @ExpSlottingContentBuilder build: @escaping () -> [C.Partial]) where P.Failure == Never {
+    self.init(trigger: trigger, build: build)
   }
 }
