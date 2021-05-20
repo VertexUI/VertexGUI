c1f994c54387e265fba2305081ae4dff756fcf32
Adrian Zimmermann <zadrian@web.de>
2021-05-20 17:33:55 +0200

    improve skia convenience functions

--- Sources/WidgetGUI/Drawing/SkiaKitExtensions/Canvas.swift
+++ Sources/WidgetGUI/Drawing/SkiaKitExtensions/Canvas.swift
@@ -2,6 +2,10 @@ import SkiaKit
 import GfxMath
 
 extension Canvas {
+  public func drawLine(_ start: DVec2, _ end: DVec2, paint: Paint) {
+    drawLine(x0: Float(start.x), y0: Float(start.y), x1: Float(end.x), y1: Float(end.y), paint: paint)
+  }
+
   public func drawRect(_ rect: DRect, _ paint: Paint) {
     drawRect(SkiaKit.Rect(x: Float(rect.min.x), y: Float(rect.min.y), width: Float(rect.size.width), height: Float(rect.size.height)), paint)
   }
